bc57b064082fbc7a763ecc076cf7b8efd67507d7
majorz <zahari@resin.io>
2018-09-24 22:17:43 +0300

    Log collector script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nm-debug-logs
-Get NetworkManager's debug logs from your Resin device
+
+NetworkManager Debug Logs Collector
---
+++ collector.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOGFILE='/mnt/data/resin-data/nm-debug-logs.txt'
+
+dbus-send --system --dest=fi.w1.wpa_supplicant1 \
+    /fi/w1/wpa_supplicant1 \
+    org.freedesktop.DBus.Properties.Set \
+    string:fi.w1.wpa_supplicant1 string:DebugTimestamp variant:boolean:true
+
+dbus-send --system --dest=fi.w1.wpa_supplicant1 \
+    /fi/w1/wpa_supplicant1 \
+    org.freedesktop.DBus.Properties.Set \
+    string:fi.w1.wpa_supplicant1 string:DebugLevel variant:string:"msgdump"
+
+dbus-send --system --dest=org.freedesktop.NetworkManager \
+    /org/freedesktop/NetworkManager \
+    org.freedesktop.NetworkManager.SetLogging \
+    string:"debug" string:""
+
+echo "===== Starting collecting logs $(date +'%F %T')..." >> $LOGFILE
+
+while true
+do
+    echo 'Collecting debug logs...'
+
+    journalctl --since "$(date --date='-1 minute' +'%F %T')" \
+        | tail -n +2 >> $LOGFILE
+
+    sleep 1m
+done
