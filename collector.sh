6bc4890a0985587349d62c74a7d932139f8da322
majorz <zahari@resin.io>
2018-10-02 17:44:39 +0300

    Collecting all debug logs from boot time

--- collector.sh
+++ collector.sh
@@ -4,12 +4,14 @@ LOGFILE='/mnt/data/resin-data/nm-debug-logs.txt'
 
 echo "===== Starting collecting logs $(date +'%F %T')..."
 
+journalctl -b >> $LOGFILE
+
 while true
 do
+    sleep 1m
+
     echo 'Collecting debug logs...'
 
     journalctl --since "$(date --date='-1 minute' +'%F %T')" \
         | tail -n +2 >> $LOGFILE
-
-    sleep 1m
 done
